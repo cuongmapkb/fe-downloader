238775e8c3a10336e8f21c488a2cbc2f534984ed
Mehran Majidi <m.majidi@digikala.com>
2019-10-03 17:10:55 +0330

    Fix bugs

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const puppeteer = require('puppeteer');
 const jsonFile  = require('jsonfile');
 const fs        = require('fs');
+const { AutoComplete } = require('enquirer');
 
 const login = async (page) => {
   await page.goto('https://frontendmasters.com/login/', { timeout: 0, waitUntil: 'domcontentloaded' });
@@ -26,18 +27,31 @@ const getLoginCookies = async () => {
 };
 
 const parseCoursesPage = async (page) => {
-  const courses = await page.$$('.MediaList > li.MediaItem');
-  console.log(courses.length);
+  const courses = await page.evaluate(() => {
+    const nodes = Array.from(document.querySelectorAll('.MediaList > li.MediaItem'));
+    
+    return nodes.map((node) => {
+      return {
+        title: node.querySelector('.title a').innerHTML,
+        url: node.querySelector('.title a').href
+      };
+    });
+  });
+
+  return courses;
 };
 
 (async () => {
   const browser = await puppeteer.launch({
     headless: true
   });
-
   const page = await browser.newPage();
   const loginCookies = await getLoginCookies();
   
+  // await page.setUserAgent(userAgent);
+  await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36');
+
+
   if (loginCookies) {
     for (cookie of loginCookies) {
       await page.setCookie(cookie);
@@ -45,9 +59,22 @@ const parseCoursesPage = async (page) => {
   } else {
     await login(page);
   }
-
+  console.log('successfully logged in!');
   
-  await page.goto('https://frontendmasters.com/courses/#all', { timeout: 0, waitUntil: 'networkidle0' });
-  await new Promise((resolve, reject) => setTimeout(() => resolve()), 10000);
-  await parseCoursesPage(page);
+  await page.goto('https://frontendmasters.com/courses/#all', { timeout: 0 });
+  await page.screenshot({ path: './ag.jpeg', type: 'jpeg' });
+  await page.waitForSelector('.MediaList > li.MediaItem');
+  const courses = await parseCoursesPage(page);
+
+
+  const prompt = new AutoComplete({
+    name: 'Course to download',
+    message: 'Select course to download',
+    limit: 50,
+    choices: courses.map(course => course.title)
+  });
+
+  prompt.run()
+    .then(answer => console.log('Answer: ', answer))
+    .catch(console.error);
 })();
